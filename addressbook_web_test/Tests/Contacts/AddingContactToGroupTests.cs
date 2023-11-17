4b14276cdd185387f20fba7e10bdb0058eaefcfd
Денис Сироткин <d.sirotkin@runetsoft.ru>
2023-11-17 05:31:27 +0300

    task 17 fix

--- addressbook_web_test/Tests/Contacts/AddingContactToGroupTests.cs
+++ addressbook_web_test/Tests/Contacts/AddingContactToGroupTests.cs
@@ -13,10 +13,35 @@ namespace AddressbookWebTest.Tests.Contacts
         [Test]
         public void AddingContactToGroupTest()
         {
-            GroupData group = GroupData.GetAll()[0];
+            var groups = GroupData.GetAll();
+            if (!groups.Any())
+            {
+                _applicationManager.Group.Create(new GroupData
+                {
+                    Name = "Test",
+                    Footer = "Test",
+                    Header = "Test",
+                });
+            }
+            List<ContactData> allContactList = ContactData.GetAll();
+            if (!allContactList.Any())
+            {
+                _applicationManager.Contact.Create(new ContactData(firstName: "Ivan2", lastName: "Ivanov2"));
+            }
+            GroupData group = groups[0];
             List<ContactData> contactsOldList = group.GetContacts();
-            ContactData contact = ContactData.GetAll().Except(contactsOldList).First();
-
+            var contacts = allContactList.Except(contactsOldList);
+            ContactData contact;
+            if (!contacts.Any())
+            {
+                _applicationManager.Contact.Create(new ContactData(firstName: "Ivan3", lastName: "Ivanov3"));
+                allContactList = ContactData.GetAll();
+                contact = allContactList.Except(contactsOldList).ToList()[0];
+            }
+            else
+            {
+                contact = contacts.First();
+            }
             _applicationManager.Contact.AddContactToGroup(contact, group);
 
             List<ContactData> contactsNewList = group.GetContacts();
--- addressbook_web_test/Tests/Contacts/DeletingContactFromGroupTests.cs
+++ addressbook_web_test/Tests/Contacts/DeletingContactFromGroupTests.cs
@@ -13,9 +13,33 @@ namespace AddressbookWebTest.Tests.Contacts
         [Test]
         public void DeletingContactFromGroupTest()
         {
-            GroupData group = GroupData.GetAll()[0];
+            var groups = GroupData.GetAll();
+            if (!groups.Any())
+            {
+                _applicationManager.Group.Create(new GroupData
+                {
+                    Name = "Test",
+                    Footer = "Test",
+                    Header = "Test",
+                });
+            }
+            List<ContactData> allContactList = ContactData.GetAll();
+            if (!allContactList.Any())
+            {
+                _applicationManager.Contact.Create(new ContactData(firstName: "Ivan2", lastName: "Ivanov2"));
+            }
+            GroupData group = groups[0];
             List<ContactData> contactsOldList = group.GetContacts();
-            ContactData contact = group.GetContacts()[0];
+            ContactData contact;
+            if (!contactsOldList.Any())
+            {
+                _applicationManager.Contact.Create(new ContactData(firstName: "Ivan3", lastName: "Ivanov3"));
+                allContactList = ContactData.GetAll();
+                contact = allContactList[0];
+                _applicationManager.Contact.AddContactToGroup(contact, group);
+                contactsOldList = group.GetContacts();
+            }
+            contact = contactsOldList[0];
 
             _applicationManager.Contact.RemoveContactFromGroup(contact, group);
 
